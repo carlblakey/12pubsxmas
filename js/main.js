9e033b08582cc5a88e71529c0edf507e732ea1cc
Carl Blakey <csblakey@gmail.com>
2021-12-04 15:15:43 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -10,7 +10,7 @@ todoList: [
 
 {"id":2,"title":"3. Shannon Arms","done":false,"location":"915 Taraval St, San Francisco, CA 94116", "map":'https://goo.gl/maps/Y8Vb8JH7xmDgb4So8', "image":'pub-03.jpg', "distance":"0.3 miles, 5 mins", "contact":"(415) 665-1223", "quote":"The problem with the world is that everyone is a few drinks behind.", "author":"― Humphrey Bogart"},
 
-{"id":3,"title":"4.  Karl's Beacon","done":false,"location":"807 Lincoln Way, San Francisco, CA 94122", "map":'https://goo.gl/maps/3BXSkJzb3KR74xuy6', "image":'pub-4.jpg', "distance":"0.6 miles, 10 mins", "contact":"(415) 592-9703", "quote":"Time is never wasted when you're wasted all the time.", "author":"― Catherine Zandonella"},
+{"id":3,"title":"4.  Karl's Beacon","done":false,"location":"807 Lincoln Way, San Francisco, CA 94122", "map":'https://goo.gl/maps/3BXSkJzb3KR74xuy6', "image":'pub-04.jpg', "distance":"0.6 miles, 10 mins", "contact":"(415) 592-9703", "quote":"Time is never wasted when you're wasted all the time.", "author":"― Catherine Zandonella"},
 
 {"id":4,"title":"5. Kezar Pub (Wings!)","done":false,"location":"1355 Taraval St, San Francisco, CA 94116", "map":'https://g.page/karlsbeaconbar?share', "image":'pub-5.jpg', "distance":"0.4 miles, 8 mins", "contact":"(415) 386-9292", "quote":"You're not drunk if you can lie on the floor without holding on.", "author":"― Dean Martin"},
 
