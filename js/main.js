5f789628a3edd2db95609fe30fd14ae33741b02f
Carl Blakey <csblakey@gmail.com>
2021-12-10 16:29:00 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -16,11 +16,11 @@ todoList: [
 
 {"id":5,"title":"6. The Riptide","done":false,"location":"3639 Taraval St, San Francisco, CA 94116", "map":'https://g.page/michaelcollinshaight?share', "image":'pub-06.jpg', "distance":"Too far, Uber time to Kezar!", "contact":"(415) 681-8433", "quote":"I feel sorry for people who don't drink. When they wake up in the morning, That's as good as they're going to feel all day.", "author":"― Frank Sinatra"},
 
-{"id":6,"title":"7. Kezar Pub (Wings)","done":false,"location":"770 Stanyan St, San Francisco, CA 94117", "map":'https://g.page/karlsbeaconbar?share', "image":'pub-5.jpg', "distance":"0.8 miles, 17 mins", "contact":"(415) 558-8375", "quote":"Fermentation may have been a better invention than fire.", "author":"― David Rains Wallace"},
+{"id":6,"title":"7. Kezar Pub (Wings)","done":false,"location":"770 Stanyan St, San Francisco, CA 94117", "map":'https://g.page/karlsbeaconbar?share', "image":'pub-5.jpg', "distance":"0.8 miles, 17 mins", "contact":"(415) 386-9292", "quote":"Fermentation may have been a better invention than fire.", "author":"― David Rains Wallace"},
 
-{"id":7,"title":"8. The Little Shamrock","done":false,"location":"807 Lincoln Way, San Francisco, CA 94122", "map":'https://goo.gl/maps/3BXSkJzb3KR74xuy6', "image":'pub-4.jpg', "distance":"371 ft, 1 min", "contact":"(415) 386-9292", "quote":"A bottle of beer contains more philosophy than all the books in the world.", "author":"― Louis Pasteur"},
+{"id":7,"title":"8. The Little Shamrock","done":false,"location":"807 Lincoln Way, San Francisco, CA 94122", "map":'https://goo.gl/maps/3BXSkJzb3KR74xuy6', "image":'pub-4.jpg', "distance":"0.2 miles, 4 mins", "contact":"(415) 661-0060", "quote":"A bottle of beer contains more philosophy than all the books in the world.", "author":"― Louis Pasteur"},
 
-{"id":8,"title":"9. Blackthorn Tavern","done":false,"location":"834 Irving St, San Francisco, CA 94122", "map":'https://goo.gl/maps/2UitTz1sUPzsZPG77', "image":'pub-2.jpg',"distance":"Too far, Uber time to Plough & the Stars!", "contact":"(415) 664-2555", "quote":"Homer no function beer well without.", "author":"- Homer Simpson"},
+{"id":8,"title":"9. Blackthorn Tavern","done":false,"location":"834 Irving St, San Francisco, CA 94122", "map":'https://goo.gl/maps/2UitTz1sUPzsZPG77', "image":'pub-2.jpg',"distance":"Too far, Uber time to Plough & the Stars!", "contact":"(415) 564-6626", "quote":"Homer no function beer well without.", "author":"- Homer Simpson"},
 
 {"id":9,"title":"10. The Plough and the Stars","done":false,"location":"116 Clement St, San Francisco, CA 94118", "map":'https://goo.gl/maps/MMv6oGqZvc2mm9299', "image":'pub-11.jpg', "distance":"2.5 miles, 52 mins (Uber/Lyft)", "contact":"(415) 255-0300", "quote":"Beer's intellectual. What a shame so many idiots drink it.", "author":"― Ray Bradbury",},
 
